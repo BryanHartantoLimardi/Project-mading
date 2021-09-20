28da820529ad3505ed92618a6f430f6a0eacfc72
Charles-Jonathan <64996896+Charles-Jonathan@users.noreply.github.com>
2021-09-20 20:42:21 +0700

    Progress Login

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -3,14 +3,24 @@
 @section('title', 'Login')
 
 @section('body-container')
+
+
+@if(session()->has('LoginError'))
+<div class="alert alert-danger alert-dismissable fade show" role="alert">
+    {{ session('LoginError') }}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-table="close"></button>
+</div>
+@endif
     <div class="container login-container">
         <h1>Let's Sign you In</h1>
         <p class="text-muted">Log in with your data that you entered <br> during your registration</p>
 
-        <form action="">
+        <form action="/login" method="post">
+        @csrf
             <div class="login-form-area">
-                <input type="text" class="login-input" placeholder="Email Address">
-                <input type="text" class="login-input" placeholder="Password">
+                
+                <input type="text" class="login-input" placeholder="Email Address" name="email" required>
+                <input type="text" class="login-input" placeholder="Password" name="password" required>
 
                 <button type="submit" class="login-button">Sign In</button>
                 <p class="m-2 text-muted login-text">Don't have an account? <a href="">Register</a></p>
